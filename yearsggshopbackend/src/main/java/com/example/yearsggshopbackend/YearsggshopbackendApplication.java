b9d43e76e1103be819240e6895a5cbbece9bcd39
years_GG <years_GG@outlook.com>
2024-11-26 22:04:19 +0800

    Initial commit

---
+++ yearsggshopbackend/src/main/java/com/example/yearsggshopbackend/YearsggshopbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.yearsggshopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YearsggshopbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YearsggshopbackendApplication.class, args);
+    }
+
+}
---
+++ yearsggshopbackend/src/test/java/com/example/yearsggshopbackend/YearsggshopbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yearsggshopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YearsggshopbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
